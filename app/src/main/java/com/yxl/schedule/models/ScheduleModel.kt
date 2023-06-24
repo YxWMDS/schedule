40dfeae9dc97030d1dd4277aeb9aec35f001bb5a
yxl <perevalov.aleksey30@gmail.com>
2023-06-24 17:55:31 +0300

    updates

---
+++ app/src/main/java/com/yxl/schedule/models/ScheduleModel.kt
@@ -0,0 +1,10 @@
+package com.yxl.schedule.models
+
+data class ScheduleModel(
+    val subName: String,
+    val profName: String,
+    val subRoom: String,
+    val subType: String,
+    val startTime: String,
+    val endTime: String
+)
